cb886acb0dc4daea5d8047271810b270d1514fdc
xinxin <xxx_cumt@163.com>
2022-01-10 00:21:23 +0800

    添加最小二乘直线拟合接口

--- calibration/include/CommonFunctions.h
+++ calibration/include/CommonFunctions.h
@@ -2,6 +2,9 @@
 
 #include <iostream>
 #include <opencv2/core.hpp>
+#include <Eigen/Core>
+#include <Eigen/SVD>
+#include <Eigen/Dense>
 
 namespace CommonStruct
 {
@@ -29,5 +32,15 @@ namespace CommonFunctions
 	
 	/*使用一组2D点拟合直线方程，并评估直线方程的误差*/
 	CommonStruct::LineFunction2D& ComputeLineFunction2D(const std::vector<cv::Point2f>& pts);
+
+	/*计算点到直线的距离*/
+	float ComputeDistanceFrom2DL2P(const CommonStruct::LineFunction2D&, const cv::Point2f&);
+
+	/*计算两条2D直线的交点*/
+	cv::Point2f& ComputeIntersectionPt(const CommonStruct::LineFunction2D&, const CommonStruct::LineFunction2D&);
+
+	/*计算数组的平均值*/
+	template <class T>
+	float Mean(std::vector<T>&);
 }
 
--- calibration/source/CommonFunctions.cpp
+++ calibration/source/CommonFunctions.cpp
@@ -45,6 +45,70 @@ namespace CommonFunctions
 
 	CommonStruct::LineFunction2D& ComputeLineFunction2D(const std::vector<cv::Point2f>& pts)
 	{
+		const int Num = pts.size();
+		Eigen::MatrixXd A = Eigen::MatrixXd(Num, 3);
+		for (size_t it = 0; it < Num; ++it) {
+			A(it, 0) = pts[it].x;
+			A(it, 1) = pts[it].y;
+			A(it, 2) = 1.;
+		}
 
+		Eigen::MatrixXd N = A.transpose()*A;
+		Eigen::EigenSolver<Eigen::Matrix<double, 3, 3>> es(N);
+		Eigen::MatrixXcd evecs = es.eigenvectors();
+		Eigen::MatrixXcd evals = es.eigenvalues();
+		Eigen::MatrixXd evalsReal = evals.real();
+		Eigen::MatrixXf::Index evalsMin;
+		evalsReal.rowwise().sum().minCoeff(&evalsMin);
+		CommonStruct::LineFunction2D line;
+		//得到最小特征值对应的特征向量
+		line.a = evecs.real()(0, evalsMin);
+		line.b = evecs.real()(1, evalsMin);
+		line.c = evecs.real()(2, evalsMin);
+		return line;
+	}
+
+	float ComputeDistanceFrom2DL2P(const CommonStruct::LineFunction2D& line, const cv::Point2f& pt)
+	{
+		const float a = line.a;
+		const float b = line.b;
+		const float c = line.c;
+		const float x = pt.x;
+		const float y = pt.y;
+		return ((std::abs)(a*x+b*y+c))/(std::sqrt(a*a+b*b));
+	}
+
+	cv::Point2f& ComputeIntersectionPt(const CommonStruct::LineFunction2D& l1, const CommonStruct::LineFunction2D& l2)
+	{
+		const float a1 = l1.a;
+		const float b1 = l1.b;
+		const float c1 = l1.c;
+		const float a2 = l2.a;
+		const float b2 = l2.b;
+		const float c2 = l2.c;
+		if (a1*b2 == a2*b1) {
+			std::cerr << "两条直线平行，无法求解交点..." << std::endl;
+			exit(-1);
+		}
+
+		Eigen::Matrix<double, 2, 2> N;
+		N(0, 0) = a1;N(0, 1) = b1;
+		N(1, 0) = a2;N(1, 1) = b2;
+		Eigen::Matrix<double, 2, 1> C;
+		C(0, 0) = -c1;C(1, 0) = -c2;
+		Eigen::Matrix<double, 2, 1> result = N.inverse()*C;
+		return cv::Point2f(result(0, 0), result(1, 0));
+	}
+
+	template <class T>
+	float Mean(std::vector<T>& vals)
+	{
+		const size_t size = vals.size();
+		if (size == 0) return 0.;
+		float meanVal = 0.;
+		for (size_t it = 0; it < size; ++it) {
+			meanVal += val[it];
+		}
+		return meanVal /= size;
 	}
 }
